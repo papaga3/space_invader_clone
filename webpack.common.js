eb503253e8362538429d77a1053e7fa795d26ba1
Huy Nguyen <ng.q.huy98@gmail.com>
2024-09-04 19:20:42 +0300

    update webpack

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.ts",
@@ -27,26 +26,11 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true
   },
   plugins: [
     new HtmlWebpackPlugin({
         template: "public/index.html"
     })
   ],
-  /*optimization: {
-    minimizer: [
-      new ImageMinimizerPlugin({
-        minimizer: {
-          implementation: ImageMinimizerPlugin.sharpMinify,
-          options: {
-            encodeOptions: {
-              // Your options for `sharp`
-              // https://sharp.pixelplumbing.com/api-output
-              // default option
-            },
-          },
-        },
-      }),
-    ],
-  },*/
 };
